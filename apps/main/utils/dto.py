4b79ca4e99d5a51ab68b0d8f16b32285f91ce0f2
Gulshan-gaur <gulssh3@gmail.com>
2023-01-11 23:35:53 +0530

    update utils.py

--- apps/main/utils/dto.py
+++ apps/main/utils/dto.py
@@ -1,8 +1,9 @@
 from flask_restx import Namespace, fields
 
-
 class UserDto:
+    # Define the namespace for the user-related operations
     api = Namespace('user', description='user related operations')
+    # Define the model for the user data
     user = api.model('user', {
         'email': fields.String(required=True, description='user email address'),
         'username': fields.String(required=True, description='user username'),
